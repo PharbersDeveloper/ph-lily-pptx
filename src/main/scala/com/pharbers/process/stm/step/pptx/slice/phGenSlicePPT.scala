d177707e096028a902c940a4fa62145bfd3d7c53
dcs <419787760@qq.com>
2018-11-28 19:29:19 +0800

    ppt content

--- src/main/scala/com/pharbers/process/stm/step/pptx/slice/phGenSlicePPT.scala
+++ src/main/scala/com/pharbers/process/stm/step/pptx/slice/phGenSlicePPT.scala
@@ -8,6 +8,8 @@ trait phGenSlicePPT {
     var df : Option[DataFrame] = None
     def genSliceDataFrame(filter : JsValue) : Unit = {
         // TODO: 从匹配表中的信息，形成一个Filter，将主表Filter出来保存到df中
+        val factory = (filter \ "factory").asOpt[String].get
+        df = Some(phLyFactory.getInstance(factory).asInstanceOf[phCommand].exec().asInstanceOf[DataFrame])
     }
 }
 
@@ -20,12 +22,12 @@ class phGenSlicePPTImpl extends phGenSlicePPT with phCommand {
         val sliders = (format \ "sliders").asOpt[List[JsValue]].get
         sliders.map { iter =>
             phLyFactory.getInstance("com.pharbers.process.stm.step.pptx.slider.phGenSliderImpl").
-                asInstanceOf[phCommand].exec(
-                    Map(
-                        "data" -> this.df,
-                        "slider" -> iter
-                    )
+                    asInstanceOf[phCommand].exec(
+                Map(
+                    "data" -> this.df,
+                    "slider" -> iter
                 )
+            )
         }
     }
 }
