f7811d57ca0eae0a97b9579fcb876fb1fccf762b
dcs <419787760@qq.com>
2019-01-19 17:10:00 +0800

    trends

--- src/main/scala/com/pharbers/process/stm/step/pptx/slider/content/phContentTable/phReportContentTable.scala
+++ src/main/scala/com/pharbers/process/stm/step/pptx/slider/content/phContentTable/phReportContentTable.scala
@@ -4,30 +4,25 @@ import com.pharbers.process.common.phCommand
 import com.pharbers.process.stm.step.pptx.slider.content.phContentTable.tableAction.actions._
 import com.pharbers.process.stm.step.pptx.slider.content.phContentTable.tableAction.{endAction, tableActionBase}
 
-class phReportContentTable extends phCommand{
+trait phContentTableCommand extends phCommand {
+    val actionList: List[tableActionBase]
+    override def exec(args: Any): Any = {
+        actionList.head.perform(args.asInstanceOf[Map[String, Any]], Some(actionList.tail))
+    }
+}
+
+class phReportContentTable extends phContentTableCommand{
     val actionList: List[tableActionBase] = phJson2ModelAction() :: phTableColArgsAction() :: phTableShowArgsAction() :: phColValueAction() ::
             phCreatShowTableAction() :: phPushTableAction() :: Nil
-
-    override def exec(args: Any): Any = {
-        actionList.head.perform(args.asInstanceOf[Map[String, Any]], Some(actionList.tail))
-    }
 }
 
-class phCityBaseTableCommonTable extends phCommand{
+class phCityBaseTableCommonTable extends phContentTableCommand{
     val actionList: List[tableActionBase] = phJson2ModelAction() :: new phCityTableColArgsAction() :: new phCityTableShowArgsAction() :: phColValueAction() ::
             new phCreatCityShowTableAction() :: phPushTableAction() :: Nil
-
-    override def exec(args: Any): Any = {
-        actionList.head.perform(args.asInstanceOf[Map[String, Any]], Some(actionList.tail))
-    }
 }
 
-//todo
-class phCityLineChartCommonTable extends phCommand{
-    val actionList: List[tableActionBase] = phJson2ModelAction() :: new phCityTableColArgsAction() :: new phCityTableShowArgsAction() :: new phColTrendsValueAction ::
-            new phCreatCityShowTrendsTableAction() :: new phPushLinChartAction() :: Nil
+class phCityLineChartCommonTable extends phContentTableCommand{
+    val actionList: List[tableActionBase] = phJson2ModelAction() :: new phCityTableColArgsAction() ::
+            new phCityTableShowArgsAction() :: new phColTrendsValueAction :: Nil
 
-    override def exec(args: Any): Any = {
-        actionList.head.perform(args.asInstanceOf[Map[String, Any]], Some(actionList.tail))
-    }
 }
